772643d01447e06d029d48665ef54b7dc5694ef1
Pragatigo12 <167645146+Pragatigo12@users.noreply.github.com>
2026-06-03 00:49:32 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import TicTacToe from './components/TicTacToe'
+
+
+
+const App = () => {
+  return (
+    <div>
+      
+      <TicTacToe/>
+    </div>
+  )
+}
+
+export default App
+//ek chota sa project banau m ?? tum bs dekhna bnao ruko video se dekh ke banaugi 
+// kro ab
+// google meet nhi kr skte didi ghr pe he
+// aab image export krni h naa >> kru error dekhta hu
---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(),tailwindcss(),
+
+  ],
+})
+
+// aisa lg rha he apko baith ke sikhana pdega
+// kyaa ??
